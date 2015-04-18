f4367960893db3fd4d9772b756ff2ee32a166b35
Rai <Midori@Rai.local>
2015-04-18 15:47:00 +0800

    for pulling

--- LunaPara/src/com/luna/para/LoginActivity.java
+++ LunaPara/src/com/luna/para/LoginActivity.java
@@ -21,7 +21,6 @@ public class LoginActivity extends BaseActivity {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				Intent intent = new Intent(ctx, MainActivity.class);
 				startActivity(intent);
 			}
